f05501e179df56a7586b486e26fd142b7e84efd6
Sahil <sahilsworkmail@gmail.com>
2024-01-30 13:44:15 +0530

    Bg Updated

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import './App.css'
 import Header from './comps/Header'
 import Footer from './comps/Footer'
 import Content from './comps/Content'
+import bgImage from "./assets/bg.webp"
 
 function App() {
 
   return (
-    <main className="main flex flex-col h-full w-full gap-[3rem] px-[3rem] py-[2rem]">
+    <main
+      className="main flex flex-col h-full w-full gap-[3rem] px-[3rem] py-[2rem] bg-no-repeat bg-bottom"
+      style={{
+        backgroundImage: `url(${bgImage})`
+      }}
+    >
       <Header className='h-fit' />
       <Content className='h-fit grow' />
       <Footer className='h-fit' />
